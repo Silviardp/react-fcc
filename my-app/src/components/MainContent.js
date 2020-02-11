5ba48712e8e3cd2493042bf56cd48070327bc9ac
Silvia Rocha <silviadprocha@gmail.com>
2020-02-11 22:12:46 +0900

    JSX to JS and back

--- my-app/src/components/MainContent.js
+++ my-app/src/components/MainContent.js
@@ -1,7 +1,22 @@
 import React from "react";
 
 function MainContent() {
-  return <main>This is the main content</main>;
+  const date = new Date();
+  const hours = date.getHours();
+  let timeOfDay;
+
+  if (hours < 12) {
+    timeOfDay = "morning";
+  } else if (hours >= 12 && hours < 17) {
+    timeOfDay = "afternoon";
+  } else {
+    timeOfDay = "night";
+  }
+
+  const firstName = "Silvia";
+  const lastName = "Rocha";
+
+  return <h1>Good {`${timeOfDay} ${firstName} ${lastName}`}!</h1>;
 }
 
 export default MainContent;
