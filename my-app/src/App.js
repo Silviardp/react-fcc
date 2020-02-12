d19305c0b420a9d88eeda2f06c40eb80610f63f3
Silvia Rocha <silviadprocha@gmail.com>
2020-02-12 17:59:52 +0900

    Mapping Components

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -7,34 +7,16 @@ import MainContent from "./components/MainContent";
 import TodoItem from "./components/TodoItem";
 import ContactCard from "./components/ContactCard";
 import Joke from "./components/Joke";
+import jokesData from "./jokesData";
 
 function App() {
-  return (
-    <div>
-      <Joke punchLine="It’s hard to explain puns to kleptomaniacs because they always take things literally." />
-
-      <Joke
-        question="What's the best thing about Switzerland?"
-        punchLine="I don't know, but the flag is a big plus!"
-      />
-      <Joke
-        question="Did you hear about the mathematician who's afraid of negative numbers?"
-        punchLine="He'll stop at nothing to avoid them!"
-      />
-      <Joke
-        question="Hear about the new restaurant called Karma?"
-        punchLine="There’s no menu: You get what you deserve."
-      />
-      <Joke
-        question="Did you hear about the actor who fell through the floorboards?"
-        punchLine="He was just going through a stage."
-      />
-      <Joke
-        question="Did you hear about the claustrophobic astronaut?"
-        punchLine="He just needed a little space."
-      />
-    </div>
-  );
+  const jokeComponents = jokesData.map(joke => {
+    return (
+      <Joke key={joke.id} question={joke.question} punchLine={joke.punchLine} />
+    );
+  });
+  // map returns a new array
+  return <div>{jokeComponents}</div>;
 }
 
 export default App;
