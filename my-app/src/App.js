cca02cc456133944cce8b385c8a742b77bd50ece
Silvia Rocha <silviadprocha@gmail.com>
2020-02-12 16:09:35 +0900

    react props

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -5,14 +5,35 @@ import Footer from "./components/Footer";
 import Header from "./components/Header";
 import MainContent from "./components/MainContent";
 import TodoItem from "./components/TodoItem";
+import ContactCard from "./components/ContactCard";
 
 function App() {
   return (
     <div className="todo-list">
-      <TodoItem />
-      <TodoItem />
-      <TodoItem />
-      <TodoItem />
+      <ContactCard
+        name="Mr. Whiskerson"
+        imgUrl="http://placekitten.com/300/200"
+        phone="(212) 555-1234"
+        email="mr.whiskaz@catnap.meow"
+      />
+      <ContactCard
+        name="Fluffykins"
+        imgUrl="http://placekitten.com/400/200"
+        phone="(212) 555-2345"
+        email="fluff@me.com"
+      />
+      <ContactCard
+        name="Destroyer"
+        imgUrl="http://placekitten.com/400/300"
+        phone="(212) 555-3456"
+        email="ofworlds@yahoo.com"
+      />
+      <ContactCard
+        name="Felix"
+        imgUrl="http://placekitten.com/200/100"
+        phone="(212) 555-4567"
+        email="thecat@hotmail.com"
+      />
     </div>
   );
 }
