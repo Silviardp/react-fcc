5a85638b616cbbbc082a926465756e001f549926
Silvia Rocha <silviadprocha@gmail.com>
2020-02-26 21:44:07 +0900

    class-based component

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -1,22 +1,13 @@
 import React from "react";
-import logo from "./logo.svg";
-import "./App.css";
-import Footer from "./components/Footer";
-import Header from "./components/Header";
-import MainContent from "./components/MainContent";
-import TodoItem from "./components/TodoItem";
-import ContactCard from "./components/ContactCard";
-import Joke from "./components/Joke";
-import jokesData from "./jokesData";
-import products from "./vschoolProducts";
-import Product from "./components/Product";
-import todosData from "./todosData";
 
-function App() {
-  const todoItems = todosData.map(item => (
-    <TodoItem key={item.id} item={item} />
-  ));
-  return <div className="todo-list">{todoItems}</div>;
+class App extends React.Component {
+  render() {
+    return (
+      <div>
+        <h1>blabla</h1>
+      </div>
+    );
+  }
 }
 
 export default App;
