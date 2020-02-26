fd28337ad612327cf4701e9c41903dbc6bc7bebd
Silvia Rocha <silviadprocha@gmail.com>
2020-02-26 21:36:41 +0900

    react todo phase 3

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -13,11 +13,10 @@ import Product from "./components/Product";
 import todosData from "./todosData";
 
 function App() {
-  const todoItems = todosData.map(item => {
-    return <TodoItem key={item.id} item={item} />;
-  });
-
-  return <div>{todoItems}</div>;
+  const todoItems = todosData.map(item => (
+    <TodoItem key={item.id} item={item} />
+  ));
+  return <div className="todo-list">{todoItems}</div>;
 }
 
 export default App;
